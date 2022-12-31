5206b4f7213458af7f58953f6763bba0f513f802
lunik1 <ch.gpg@themaw.xyz>
2022-12-31 14:31:30 +0000

    I'm me

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "My personal NUR repository";
+  description = "lunik1's NUR repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   outputs = { self, nixpkgs }:
     let
