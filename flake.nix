50661966bb184fb9e60006dd3fc0132dea4289b6
lunik1 <ch.gpg@themaw.xyz>
2023-02-03 14:34:03 +0000

    add devShell

--- flake.nix
+++ flake.nix
@@ -4,6 +4,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs-lint = {
+      url = "github:nix-community/nixpkgs-lint";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        utils.follows = "flake-utils";
+      };
+    };
   };
 
   outputs = inputs@{ self, ... }:
@@ -11,10 +18,22 @@
     flake-utils.lib.eachSystem [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "armv6l-linux" "armv7l-linux" ] (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system; overlays = [ nixpkgs-lint.overlays.default ]; };
       in
       {
         packages = import ./default.nix { inherit pkgs; };
+
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              nix-update
+              nixpkgs-fmt
+              nixpkgs-lint
+              nurl
+              rnix-lsp
+              statix
+            ];
+          };
       }
     );
 }
