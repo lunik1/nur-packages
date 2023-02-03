a169fbb0af69631de6ff416a7bf405afdb6fb6ea
lunik1 <ch.gpg@themaw.xyz>
2023-02-03 13:29:02 +0000

    flake.nix: rewrite to use flake-utils

--- flake.nix
+++ flake.nix
@@ -1,21 +1,20 @@
 {
   description = "lunik1's NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
-    let
-      systems = [
-        "x86_64-linux"
-        "i686-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "armv6l-linux"
-        "armv7l-linux"
-      ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      packages = forAllSystems (system: import ./default.nix {
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs@{ self, ... }:
+    with inputs;
+    flake-utils.lib.eachSystem [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "armv6l-linux" "armv7l-linux" ] (
+      system:
+      let
         pkgs = import nixpkgs { inherit system; };
-      });
-    };
+      in
+      {
+        packages = import ./default.nix { inherit pkgs; };
+      }
+    );
 }
