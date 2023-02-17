622a48edb73034dd7db7a4742a8a01b14f8d38ea
lunik1 <ch.gpg@themaw.xyz>
2023-02-17 16:57:35 +0000

    fsrcnnx-x2-[8|16]-0-4-1: init at 1.1

--- default.nix
+++ default.nix
@@ -6,6 +6,8 @@
   overlays = import ./overlays;
 
   amazing-marvin = pkgs.callPackage ./pkgs/amazing-marvin { };
-  xcompose = pkgs.callPackage ./pkgs/xcompose { };
+  fsrcnnx-x2-8-0-4-1 = pkgs.callPackage ./pkgs/mpv-shaders/fsrcnnx { variant = "8-0-4-1"; };
+  fsrcnnx-x2-16-0-4-1 = pkgs.callPackage ./pkgs/mpv-shaders/fsrcnnx { variant = "16-0-4-1"; };
   quality-menu = pkgs.callPackage ./pkgs/mpv-scripts/quality-menu { };
+  xcompose = pkgs.callPackage ./pkgs/xcompose { };
 }
