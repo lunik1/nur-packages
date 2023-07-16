3f7d3fa48082742efd809450a236a72270cc67f6
lunik1 <ch.gpg@themaw.xyz>
2023-07-16 12:15:28 +0100

    bach: init at 0.3

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@
 
 {
   amazing-marvin = pkgs.callPackage ./pkgs/amazing-marvin { };
+  bach = pkgs.callPackage ./pkgs/bach { };
   fsrcnnx-x2-8-0-4-1 = pkgs.callPackage ./pkgs/mpv-shaders/fsrcnnx { variant = "8-0-4-1"; };
   fsrcnnx-x2-16-0-4-1 = pkgs.callPackage ./pkgs/mpv-shaders/fsrcnnx { variant = "16-0-4-1"; };
   krig-bilateral = pkgs.callPackage ./pkgs/mpv-shaders/krig-bilateral { };
