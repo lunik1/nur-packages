b36416dccd5486a9d6a9ecb544126d288a2243a4
lunik1 <ch.gpg@themaw.xyz>
2023-12-02 19:06:31 +0000

    fix error: expected a derivation on "nix flake show"

--- default.nix
+++ default.nix
@@ -1,10 +1,6 @@
 { pkgs ? import <nixpkgs> { } }:
 
 {
-  lib = import ./lib { inherit pkgs; };
-  modules = import ./modules;
-  overlays = import ./overlays;
-
   amazing-marvin = pkgs.callPackage ./pkgs/amazing-marvin { };
   fsrcnnx-x2-8-0-4-1 = pkgs.callPackage ./pkgs/mpv-shaders/fsrcnnx { variant = "8-0-4-1"; };
   fsrcnnx-x2-16-0-4-1 = pkgs.callPackage ./pkgs/mpv-shaders/fsrcnnx { variant = "16-0-4-1"; };
