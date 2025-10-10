64dd29826479dd11d1be76f514e46defe9233c83
lunik1 <ch.gpg@themaw.xyz>
2025-10-10 23:53:26 +0100

    use nixfmt for formatting

--- flake.nix
+++ flake.nix
@@ -17,44 +17,52 @@
     };
   };
 
-  outputs = inputs@{ self, ... }:
+  outputs =
+    inputs@{ self, ... }:
     with inputs;
     {
       nixosModules = import ./modules;
-    } //
-    flake-utils.lib.eachSystem [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "armv6l-linux" "armv7l-linux" ] (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; overlays = [ nixpkgs-lint.overlays.default ]; };
-      in
-      {
-        packages = import ./default.nix { inherit pkgs; };
-
-        formatter = pkgs.nixpkgs-fmt;
-
-        devShells.default = with pkgs;
-          mkShell {
-            inherit (self.checks.${system}.pre-commit-check) shellHook;
-            buildInputs = [
-              nix-output-monitor
-              nix-update
-              nixpkgs-fmt
-              nixpkgs-lint
-              nurl
-              nil
-              statix
-            ];
-          };
-
-        checks = {
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              nixpkgs-fmt.enable = true;
-              statix.enable = true;
+    }
+    //
+      flake-utils.lib.eachSystem
+        [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "armv6l-linux" "armv7l-linux" ]
+        (
+          system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ nixpkgs-lint.overlays.default ];
             };
-          };
-        };
-      }
-    );
+          in
+          {
+            packages = import ./default.nix { inherit pkgs; };
+
+            formatter = pkgs.nixfmt;
+
+            devShells.default =
+              with pkgs;
+              mkShell {
+                inherit (self.checks.${system}.pre-commit-check) shellHook;
+                buildInputs = [
+                  nix-output-monitor
+                  nix-update
+                  nixfmt
+                  nixpkgs-lint
+                  nurl
+                  nil
+                  statix
+                ];
+              };
+
+            checks = {
+              pre-commit-check = pre-commit-hooks.lib.${system}.run {
+                src = ./.;
+                hooks = {
+                  nixfmt-rfc-style.enable = true;
+                  statix.enable = true;
+                };
+              };
+            };
+          }
+        );
 }
